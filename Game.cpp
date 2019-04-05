db3b64903c76fa9e39c84f2a0bb784a4aefef9af
Víctor Nedyar <0Nedyar0@gmail.com>
2019-04-05 12:40:28 +0200

    Clase level preparada

--- Game.cpp
+++ Game.cpp
@@ -1,18 +1,38 @@
+// Will deprecate
 #include <SFML/Graphics.hpp>
-#include <Character.h>
+
 #include <iostream>
+#include <State.h>
+#include <Level.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
+State* currentState;
+
+void setState(State* newState) {
+    currentState = newState;
+}
+
+void handleEvents() {
+
+}
+
+void update() {
+
+}
+
+void render() {
+
+}
+
 int main()
 {
-
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(WIDTH, HEIGHT), "TurtleWars");
     app.setFramerateLimit(60);
 
-    Character *player = new Character(1);
+    setState(Level::instance());
 
     // Start the game loop
     while (app.isOpen())
@@ -27,43 +47,17 @@ int main()
 
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            player->walk(true);
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            player->walk(false);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            player->jump();
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            player->crouch();
-        else
-            player->standUp();
-
-
-
-        player->update();
-
+        currentState->handleEvents();
+        currentState->update();
 
         // Clear screen
         app.clear(sf::Color(200,200,200,255));
         // Draw the sprite
         //player->draw(app);
-        player->draw(app);
+        currentState->draw(app);
         // Update the window
         app.display();
     }
 
     return EXIT_SUCCESS;
-}
-
-void handleEvents() {
-
-}
-
-void update() {
-
-}
-
-void render() {
-
 }
